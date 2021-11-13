8c680acc6c9fedbcd26acd4a5ee1f1b350b8ac78
DESKTOP-UJO5UUV\DELL <yachita.poohping@mail.kmutt.ac.th>
2021-11-14 02:33:51 +0700

    stackandqueueHW3jaa

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                char instruction = char.Parse(Console.ReadLine());
+                if (instruction == '?')
+                {
+                    break;
+                }
+                else { }
+                char data = char.Parse(Console.ReadLine());
+                CPU list = new CPU(instruction, data);
+            }
+        }
+    }
+    class CPU
+    {
+        public char instruction;
+        public char data;
+        public CPU(char instructionValue, char dataValue)
+        {
+            instruction = instructionValue;
+            data = dataValue;
+        }
+    }
+    
+}
